7c6670cf2e5b78db4bd4d0cad623bc7682a86564
chenhj <chenhj@belink.com>
2019-08-07 18:30:05 +0800

    基本异常类修改

--- publics/public-base/src/main/java/com/brigeintelligent/base/basemethod/BaseException.java
+++ publics/public-base/src/main/java/com/brigeintelligent/base/basemethod/BaseException.java
@@ -16,6 +16,10 @@ public class BaseException extends RuntimeException {
     private int code;
     private String msg;
 
+    public BaseException(String msg) {
+        this.msg = msg;
+    }
+
     public BaseException(int code, String msg) {
         super(msg);
         this.code = code;
